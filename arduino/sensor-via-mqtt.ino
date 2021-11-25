fcb9e809bb6828442fbf0d156e6564dea1f06cd6
John Rihacek <jrihacek@gmail.com>
2021-11-25 10:44:25 -0500

    sending temperature to mqtt broker

--- arduino/sensor-via-mqtt.ino
+++ arduino/sensor-via-mqtt.ino
@@ -17,16 +17,66 @@ const char* mqtt_pw = MQTT_PW;
 #include <Adafruit_Sensor.h>
 #include <DHT.h>
 
+// MQTT
+#include <PubSubClient.h>
+#include <ArduinoJson.h> //https://github.com/bblanchon/ArduinoJson/wiki/API%20Reference
+
+#define MQTT_VERSION MQTT_VERSION_3_1_1
+const char* MQTT_CLIENT_ID = "bsmnt_computers";
+const char* MQTT_SERVER_IP = "192.168.86.202";
+const uint16_t MQTT_SERVER_PORT = 1883;
+
+const char* MQTT_TOPIC_A = "bsmnt/temperature";
+const char* MQTT_TOPIC_B = "bsmnt/humidity";
+
+
 char daysOfTheWeek[7][12] = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
 const long utcOffsetInSeconds = -18000; //UTC -5.00 (-5 * 60 sec in min * 60 min in hr = -18000)
 
 WiFiUDP wifi_udp;
 
 NTPClient timeClient(wifi_udp, "pool.ntp.org", utcOffsetInSeconds);
+WiFiClient espClient;
+PubSubClient mqttClient(espClient);
 
 #define DHT11_PIN D5
 #define DHTTYPE DHT11 
 DHT dht = DHT(DHT11_PIN, DHTTYPE);
+
+// function called to publish the temperature and the humidity
+void publishData(float p_temperature, float p_humidity) {
+  // create a JSON object
+  StaticJsonDocument<200> jsonBuffer;
+  JsonObject root = jsonBuffer.to<JsonObject>(); // clear and replace with a new JsonObject
+  root["temperature"] = (String)p_temperature;
+  root["humidity"] = (String)p_humidity;
+  serializeJsonPretty(root, Serial);
+  Serial.println("");
+  char data[200];
+  serializeJson(root,data);
+  mqttClient.publish(MQTT_TOPIC_A, data, true);
+}
+
+// function called when a MQTT message arrives
+void callback(char* p_topic, byte* p_payload, unsigned int p_length) {
+}
+
+void reconnect() {
+  // Loop until we're reconnected
+  while (!mqttClient.connected()) {
+    Serial.println("INFO: Attempting MQTT connection...");
+    // Attempt to connect
+    if (mqttClient.connect(MQTT_CLIENT_ID, mqtt_user, mqtt_pw)) {
+      Serial.println("INFO: connected");
+    } else {
+      Serial.print("ERROR: failed, rc=");
+      Serial.print(mqttClient.state());
+      Serial.println("DEBUG: try again in 5 seconds");
+      // Wait 5 seconds before retrying
+      delay(5000);
+    }
+  }
+}
 
 void setup() {
   Serial.begin(9600);
@@ -37,10 +87,17 @@ void setup() {
   timeClient.begin();
 
   dht.begin();
+
+  mqttClient.setServer(MQTT_SERVER_IP, MQTT_SERVER_PORT);
+  mqttClient.setCallback(callback);
 }
 
 void loop(){
   timeClient.update();
+
+  if (!mqttClient.connected()) {
+    reconnect();
+  }
 
   int hh = timeClient.getHours();
   int mm = timeClient.getMinutes();
@@ -75,5 +132,12 @@ void loop(){
   Serial.println("% Humidity");
   Serial.println();
 
-  delay(5000); //300000 = 5 mins
+  if (isnan(h) || isnan(f)) {
+    Serial.println("ERROR: Failed to read from DHT sensor!");
+    return;
+  } else {
+    publishData(t, h);
+  }
+
+  delay(10000); //300000 = 5 mins
 }
